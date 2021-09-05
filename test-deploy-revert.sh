13b162507ad25247276276f8bb349adb4bafea59
Michael Moser <moser.michael@gmail.com>
2021-09-05 22:37:56 +0300

    scripts for testing deployment

---
+++ test-deploy-revert.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# test deploy from current dir, to test the thing.
+
+set -e
+
+if [[ ! -d $HOME/.vim-bak ]]; then
+
+    echo "can't revert test deploy, directory $HOME/.vim-bak does not exist"
+    exit 1
+fi
+
+if [[ ! -f $HOME/.vimrc-bak ]]; then
+
+    echo "can't revert test deploy, file $HOME/.vimrc-bak already exists"
+    exit 1
+fi
+
+rm -f $HOME/.vimrc
+rm -rf $HOME/.vim
+
+mv $HOME/.vim-bak $HOME/.vim
+mv $HOME/.vimrc-bak $HOME/.vimrc
+
+
+
+
---
+++ test-deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# test deploy from current dir, to test the thing.
+
+set -e
+
+if [[ -d $HOME/.vim-bak ]]; then
+
+    echo "can't test deploy, directory $HOME/.vim-bak already exists"
+    exit 1
+fi
+
+if [[ -f $HOME/.vimrc-bak ]]; then
+
+    echo "can't test deploy, file $HOME/.vimrc-bak already exists"
+    exit 1
+fi
+
+
+mv $HOME/.vim $HOME/.vim-bak
+mv $HOME/.vimrc $HOME/.vimrc-bak
+
+mkdir -p  $HOME/.vim/pack/vendor/start/gitblame/
+cp -rf . $HOME/.vim/pack/vendor/start/gitblame/
+
+
+
+
